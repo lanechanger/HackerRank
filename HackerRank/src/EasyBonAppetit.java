f766ed023b91099287576c904a1380fff3d42d22
lanechanger <jeremy_ng_86@live.ca>
2018-04-09 14:15:15 -0400

    Fixing Bon Appetit and Migratory Birds as their code were reversed

--- HackerRank/src/EasyBonAppetit.java
+++ HackerRank/src/EasyBonAppetit.java
@@ -1,9 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/*https://www.hackerrank.com/challenges/bon-appetit/problem
+Practice > Algorithms > Implementation > Bon Appétit
+by Jeremy Ng (lanechanger)*/
 
 public class EasyBonAppetit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+        Scanner in = new Scanner(System.in);
+		int totalItems = in.nextInt();
+		int allergicIndex = in.nextInt();
+		int sum = 0;
+		for (int i = 0; i < totalItems; i++) {
+			int current = in.nextInt();
+			// add the number if it wasn't what Anna was allergic to
+			sum += (i != allergicIndex) ? current : 0;
+        }
+		int charged = in.nextInt();
+		int actual = sum / 2;
+		// delta(charged, actual) if brian overcharged, "Bon Appetit" otherwise
+		System.out.println((actual == charged) ? "Bon Appetit" : charged - actual);
+    }
 
 }
--- HackerRank/src/EasyMigratoryBirds.java
+++ HackerRank/src/EasyMigratoryBirds.java
@@ -1,23 +1,55 @@
 import java.util.Scanner;
-/*https://www.hackerrank.com/challenges/bon-appetit/problem
-Practice > Algorithms > Implementation > Bon Appétit
+/*https://www.hackerrank.com/challenges/migratory-birds/problem
+Practice > Algorithms > Implementation > Migratory Birds
 by Jeremy Ng (lanechanger)*/
 
 
 public class EasyMigratoryBirds {
+	static int migratoryBirds(int n, int[] ar) {
+		// Complete this function
+		final int BIRDS = 5;
+		int[] all = new int[BIRDS];
+		int max = Integer.MIN_VALUE;
+		int maxIndex = 0;
+
+		// restructure this potentially massive array
+		for (int i = 0; i < n; i++) {
+			switch (ar[i]) {
+			case 1:
+				all[0]++;
+				break;
+			case 2:
+				all[1]++;
+				break;
+			case 3:
+				all[2]++;
+				break;
+			case 4:
+				all[3]++;
+				break;
+			default:
+				all[4]++;
+			}
+		}
+
+		for (int i = 0; i < BIRDS; i++) {
+			if (all[i] > max) {
+				max = all[i];
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex + 1;
+	}
+
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-		int totalItems = in.nextInt();
-		int allergicIndex = in.nextInt();
-		int sum = 0;
-		for (int i = 0; i < totalItems; i++) {
-			int current = in.nextInt();
-			// add the number if it wasn't what Anna was allergic to
-			sum += (i != allergicIndex) ? current : 0;
+		int n = in.nextInt();
+		int[] ar = new int[n];
+		for (int ar_i = 0; ar_i < n; ar_i++) {
+			ar[ar_i] = in.nextInt();
         }
-		int charged = in.nextInt();
-		int actual = sum / 2;
-		// delta(charged, actual) if brian overcharged, "Bon Appetit" otherwise
-		System.out.println((actual == charged) ? "Bon Appetit" : charged - actual);
+		int result = migratoryBirds(n, ar);
+		System.out.println(result);
     }
 }
